654a20a39c360d286fefd5b6094be2228491b384
magaly-rosales <magaly.rosales@tecsup.edu.pe>
2026-08-25 18:40:47 -0500

    Delete StudentController.java

--- StudentController.java
+++
@@ -1,6 +0,0 @@
-public class StudentController {
-    public static void main(String[] args) {
-        System.out.println("Student Controller");
-        System.out.println("Actualizado por el responsable"); 
-    }
-}
