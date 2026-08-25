5330ee794bad430fdfa266314127facfdf983ab9
magaly-rosales <magaly.rosales@tecsup.edu.pe>
2026-08-25 18:24:43 -0500

    Fix print statement formatting in StudentControllerAgrega mensaje de log en el método main de StudentController

--- StudentController.java
+++ StudentController.java
@@ -1,6 +1,6 @@
 public class StudentController {
     public static void main(String[] args) {
         System.out.println("Student Controller");
-        System.out.println("Actualizado por el responsable");
+        System.out.println("Actualizado por el responsable"); 
     }
 }
